08593cfce35d61be370dcb3ce3c9cc4d29591672
briyanchiyang <brian.chiang.25@dartmouth.edu>
2024-02-17 09:21:27 -0500

    Added user login functionality
    User can now log into spotify on web! Next: work on functionality afterwards

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import { useEffect, useState } from "react";
 import './App.css';
 import OpenAI from "openai";
 
-
+const open_ai_key = process.env.REACT_APP_OPENAI
 const spotify_client_id = process.env.REACT_APP_SPOTIFY_CLIENT_ID
 const spotify_client_secret = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET
+const redirect_uri = "http://localhost:3000/auth/callback"
 
 function App() {
   console.log(process.env);
+  console.log(spotify_client_id)
 
   // Image generation
-  const openai = new OpenAI({apiKey: process.env.REACT_APP_OPENAI, dangerouslyAllowBrowser: true});
+  const openai = new OpenAI({apiKey: open_ai_key});
   const [result, setResult] = useState(0);
+
   const generateImage = async () => {
     const res = await openai.images.generate({
       prompt: 'person',
@@ -20,31 +23,40 @@ function App() {
       size: "512x512",
     });
 
-    const completion = await openai.chat.completions.create({
-      messages: [{ role: "system", content: "You are a helpful assistant." }],
-      model: "gpt-3.5-turbo",
-    });
-  
     setResult(res.data[0].url);
   };
 
+
   // Song generation
-  useEffect(() => { // syntax for running only once
+  const [accessToken, setAccessToken] = useState("");
 
-    // API access token
-    var authParameters = {
-      method: 'POST',
-      headers: {
-        'Content-Type': "application/x-www-form-urlencoded"
-      },
+  const login = () => {
+    // Auth request
+    var url = "https://accounts.spotify.com/authorize"; // Base url
+    url += '?response_type=token';
+    url += '&client_id=' + encodeURIComponent(spotify_client_id);
+    //url += '&scope=' + encodeURIComponent(["user-top-read"]);
+    url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+    //url += '&state=' + encodeURIComponent(state);
+    window.location = url;
 
-      // Boilerplate for passing client id and secret
-      body: 'grant_type=client_credentials&client_id=' + spotify_client_id + '&client_secret=' + spotify_client_secret
-    }
-    fetch("https://accounts.spotify.com/api/token", authParameters)
-      .then(result => result.json())
-      .then(data => console.log(data))
-  }, [])
+  }
+
+  //useEffect(() => { // syntax for running only once
+  //  // API access token
+  //  var authParameters = {
+  //    method: 'POST',
+  //    headers: {
+  //      'Content-Type': "application/x-www-form-urlencoded"
+  //    },
+
+  //    // Boilerplate for passing client id and secret
+  //    body: 'grant_type=client_credentials&client_id=' + spotify_client_id + '&client_secret=' + spotify_client_secret
+  //  }
+  //  fetch("https://accounts.spotify.com/api/token", authParameters)
+  //    .then(result => result.json())
+  //    .then(data => setAccessToken(data.access_token)) // !!error handling!!
+  //}, [])
 
 
   return (
@@ -63,6 +75,7 @@ function App() {
           Learn React
         </a>
         <button>Generate an Image</button>
+        <button onClick={login}>Login to Spotify!</button>
         {result.length > 0 ? (
           <img className="result-image" src={result} alt="result" />
         ) : (
