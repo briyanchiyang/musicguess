b9b3344196f102ca04ee0c0d932992c1d17099d6
briyanchiyang <brian.chiang.25@dartmouth.edu>
2024-02-17 11:51:03 -0500

    Added access token acquisition, playlist search
    a) Gets access token from login request, b) search() does GET request of current user's playilsts. Next: scrape JSON file for songs

--- src/App.js
+++ src/App.js
@@ -8,39 +8,75 @@ const spotify_client_id = process.env.REACT_APP_SPOTIFY_CLIENT_ID
 const spotify_client_secret = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET
 const redirect_uri = "http://localhost:3000/auth/callback"
 
-function App() {
-  console.log(process.env);
-  console.log(spotify_client_id)
+const openai = new OpenAI({apiKey: open_ai_key, dangerouslyAllowBrowser: true});
 
-  // Image generation
-  const openai = new OpenAI({apiKey: open_ai_key});
+// Get response query params for subsequent calls
+// http://localhost:3000/auth/callback#access_token=BQDKGqrNHIbFuKM7DFKAl-gpAHNuqD5qjSgmKSqZdjEubk_84XwkQIT6FSkggfCUxk2t8lAyVKKEO9lAtoZwR8kSYIrbBus0QpByv81kcZxzfZcPn7w9zJJR_zBjmZ5QKlQ2D1LES4qRuuAH0UaJF4lrDD0XKdjxDubWGfMXrA&token_type=Bearer&expires_in=3600
+
+const getAccessToken = (hash) => {
+  const param_string = hash.substring(1).split("&");
+  const params = new Map();
+
+  for (let i = 0; i < param_string.length; i++){
+    const [key, val] = param_string[i].split("=");
+    params[key] = val;
+  }
+  return params;
+}
+
+
+function App() {
+  var params = [];
+  useEffect(() => {
+    if(window.location.hash) {
+      params = getAccessToken(window.location.hash);
+      console.log("Params2" + params);
+    }
+  });
+  console.log("Params" + params);
+
   const [result, setResult] = useState(0);
 
+  // Image generation
   const generateImage = async () => {
     const res = await openai.images.generate({
       prompt: 'person',
       n: 1,
       size: "512x512",
     });
-
     setResult(res.data[0].url);
   };
 
 
   // Song generation
   const [accessToken, setAccessToken] = useState("");
-
   const login = () => {
     // Auth request
     var url = "https://accounts.spotify.com/authorize"; // Base url
     url += '?response_type=token';
     url += '&client_id=' + encodeURIComponent(spotify_client_id);
-    //url += '&scope=' + encodeURIComponent(["user-top-read"]);
+    url += '&scope=' + encodeURIComponent(["user-top-read"]);
     url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
     //url += '&state=' + encodeURIComponent(state);
     window.location = url;
 
   }
+
+  async function search() {
+    console.log("Searching", params.access_token);
+
+    var playlist = await fetch("https://api.spotify.com/v1/me/playlists", {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + params.access_token
+      }
+    });
+
+    const data = await playlist.json();
+    console.log(data);
+  }
+
 
   //useEffect(() => { // syntax for running only once
   //  // API access token
@@ -76,6 +112,7 @@ function App() {
         </a>
         <button>Generate an Image</button>
         <button onClick={login}>Login to Spotify!</button>
+        <button onClick={search}>Playlist retrieval</button>
         {result.length > 0 ? (
           <img className="result-image" src={result} alt="result" />
         ) : (
